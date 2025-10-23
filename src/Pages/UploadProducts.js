a096a470d588a03cd9a3011a0c96d33226e60567
Arole01 <ajosedavidayobami@gmail.com>
2025-10-23 14:37:21 +0100

    formData

--- src/Pages/UploadProducts.js
+++ src/Pages/UploadProducts.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as Yup from "yup"
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 export const UploadProducts = () =>{
 
@@ -21,9 +23,30 @@ export const UploadProducts = () =>{
 } = useForm({
     resolver: yupResolver(schema),  
     });
+
+    const fetchCategory = async()=> {
+        try {
+            const {data} = await axios.get("https://davidbackend-ts7d.onrender.com/api/products")
+            setCategory(data)
+        } catch (error) {
+            console.log(error.message)
+            toast.error(error.message)
+        }
+    }
+
+    const UploadProduct = async (data)=>{
+        const info = new FormData()
+        info.append("name",data.name)
+        info.append("description",data.description)
+        info.append("price",data.price)
+        info.append("stock",data.stock)
+        info.append("category",data.category)
+    }
     return (
     <div>
-        
+        <form onSubmit={handleSubmit(Submit)}>
+        <p>Upload your product below</p>
+        </form>
     </div>
     )
 }
